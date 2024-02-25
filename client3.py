2c358544fbfdbb9289054d8da03bfe165a960f7b
AerioJobin <127238136+AerioJobin@users.noreply.github.com>
2024-02-25 11:27:52 +0530

    Update client3.py

--- client3.py
+++ client3.py
@@ -5,7 +5,7 @@ import urllib.request
 # Server API URLs
 QUERY = "http://localhost:8080/query?id={}"
 
-# 500 server request
+# 500 server requests
 N = 500
 
 
@@ -14,7 +14,7 @@ def getDataPoint(quote):
     stock = quote['stock']
     bid_price = float(quote['top_bid']['price'])
     ask_price = float(quote['top_ask']['price'])
-    price = (bid_price + ask_price) / 2
+    price = (bid_price + ask_price) / 2  # Calculate price using the average of bid and ask prices
     return stock, bid_price, ask_price, price
 
 
@@ -27,21 +27,22 @@ def getRatio(price_a, price_b):
 
 # Main
 if __name__ == "__main__":
+    prices = {}  # Dictionary to store stock prices
     # Query the price once every N seconds.
     for _ in iter(range(N)):
         quotes = json.loads(urllib.request.urlopen(QUERY.format(random.random())).read())
 
-        prices = {}  # Dictionary to store prices of different stocks
         for quote in quotes:
-            stock, _, _, price = getDataPoint(quote)
-            prices[stock] = price
+            stock, bid_price, ask_price, price = getDataPoint(quote)
+            print("Quoted %s at (bid:%s, ask:%s, price:%s)" % (stock, bid_price, ask_price, price))
+            prices[stock] = price  # Store the price in the prices dictionary
 
-        # Check if data for both stocks is available
-        if 'ABC' in prices and 'DEF' in prices:
-            ratio = getRatio(prices["ABC"], prices["DEF"])
-            if ratio is not None:
-                print("Ratio ABC/DEF: %.2f" % ratio)
-            else:
-                print("Unable to calculate ratio: Division by zero.")
+    # Calculate and print the ratio
+    if 'ABC' in prices and 'DEF' in prices:
+        ratio = getRatio(prices["ABC"], prices["DEF"])
+        if ratio is not None:
+            print("Ratio ABC/DEF: %.2f" % ratio)
         else:
-            print("Data not available for both ABC and DEF.")
+            print("Unable to calculate ratio: Division by zero.")
+    else:
+        print("Data not available for both ABC and DEF.")
